4992eeb233f21e9aa220928cee0f0dfc7058350f
CarlosTercero <carlosfarres2004@gmail.com>
2023-10-16 16:45:01 +0200

    ProyectoAcabado

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,19 @@ import 'package:flutter/material.dart';
 void main() {
   return runApp(MaterialApp(
     home: Scaffold(
-      appBar: AppBar(
-        title: Text("Hello World!"),
-      ),
+      appBar: AppBar(title: const Text("Hello World!")),
+      body: Column(
+          children: const [
+            SizedBox(
+              width: double.infinity,
+              height: 50,
+            ),
+            Text(
+              "Carlos Farres Lozano",
+              style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+            ),
+          ],
+       ),
     ),
   ));
 }
